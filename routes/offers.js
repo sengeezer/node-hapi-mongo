c510fe89578b956c3f2b45f702e7a86aea99e86f
Fabio Albertin <albertin@gmail.com>
2020-10-22 21:06:11 +0100

    Added PATCH route

--- routes/offers.js
+++ routes/offers.js
@@ -27,7 +27,7 @@ const routesOffers = {
     server.route({  
       method: 'GET',
       path: '/offers/{id}',
-      handler: function (request, h) {
+      handler: (request, h) => {
         db.offers.findOne({
           _id: request.params.id
         }, (err, doc) => {
@@ -62,7 +62,7 @@ const routesOffers = {
         notes: 'Create an offer',
         tags: ['api']
       },
-      handler: function (request, h) {
+      handler: (request, h) => {
         const offer = request.payload;
 
         // Create an id
@@ -74,6 +74,41 @@ const routesOffers = {
           }
 
           return h.response(offer);
+        });
+      }
+    });
+
+    server.route({  
+      method: 'PATCH',
+      path: '/offers/{id}',
+      options: {
+        auth: false,
+        validate: {
+          payload: Joi.object({
+            name: Joi.string().required().note('Offer name'),
+            expires: Joi.date().greater('now').optional().note('Offer expiration date')
+          }).required().min(1),
+          failAction: handleError
+        },
+        description: 'Update offer',
+        notes: 'Update an offer',
+        tags: ['api']
+      },
+      handler: (request, h) => {
+        db.offers.update({
+          _id: request.params.id
+        }, {
+          $set: request.payload
+        }, (err, result) => {
+          if (err) {
+            return h.response(Boom.wrap(err, 'Internal MongoDB error'));
+          }
+
+          if (result.n === 0) {
+            return h.response(Boom.notFound());
+          }
+
+          h.response().code(204);
         });
       }
     });
